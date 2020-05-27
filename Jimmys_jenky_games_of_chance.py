136a8b6a89c337d300171710e40ead7a715b7cd2
jamesbarnett19 <65197907+HCKYJOC@users.noreply.github.com>
2020-05-27 16:55:04 -0700

    Added Cho-Han

--- Jimmys_jenky_games_of_chance.py
+++ Jimmys_jenky_games_of_chance.py
@@ -12,6 +12,7 @@ def coin_flip(bet, guess, total):
   if bet > total:
     print("You don't have enough money to make that bet!")
     return 0
+  print("Coin Flip!")
   print("You have bet $" + str(bet) + " and guessed " + str(guess) + ".")
   print()
   print("--------------------------------------------")
@@ -49,11 +50,47 @@ def coin_flip(bet, guess, total):
     print()
     return -bet
 
+def cho_han(odd_even, bet, total):
+  if bet <= 0:
+    print("You must bet more than $0")
+    return 0
+  if bet > total:
+    print("You don't have enough money to make that bet!")
+    return 0
+  print("CHO_HAN!")
+  print("You have bet $" + str(bet) + " and guessed " + str(odd_even) + ".")
+  print()
+  print("--------------------------------------------")
+  print()
+
+  result = random.randint(2, 12)
+  if result == 2 or result == 4 or result == 6 or result == 8 or result == 10 or result == 12:
+    print("The dice add up to " + str(result) + "!")
+    print("Even!")
+    print()
+  else:
+    print("The dice add up to " + str(result) + "!")
+    print("Odd!")
+    print()
+
+  if (odd_even == "Even" and (result == 2 or result == 4 or result == 6 or result == 8 or result == 10 or result == 12)):
+    print("You win $" + str(bet))
+    return +bet
+  if (odd_even == "Odd" and (result == 3 or result == 5 or result == 7 or result == 9 or result == 11)):
+    print("You win $" + str(bet))
+    return +bet
+  else:
+    print("You lose $" + str(bet))
+    return -bet
+
+
 #Call your game of chance functions here
-#total += coin_flip(0, "Heads", total)
-#print("Your New Total is: $" + str(total))
-#print("************")
+
 total += coin_flip(5, "Heads", total)
+print("Your New Total is: $" + str(total))
+print("************")
+
+total += cho_han("Odd", 5, total)
 print("Your New Total is: $" + str(total))
 print("************")
 #total += coin_flip(5, "Heads", total)
